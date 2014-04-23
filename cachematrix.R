a9b0dd42b049d1bae2fa40197f90df4afb9f98ce
zuowenist <xukun4st@outlook.com>
2014-04-23 21:57:26 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Besically clone from makeVector() and cachemean(),add a bit of warning functions
+## that can distinguish two types of uninversiable matrix.
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        if(dim(x)[1]!=dim(x)[2]){m<-0}  
+        # judge if x is a square matrix.If not,m<-0 then cacheSolve() will output corresponding warning massage. 
+        else if(det(x)==0){m<-1}        
+        # judge if the det of x is zero.If so,m<-1,then cacheSolve() will output corresponding warning massage.
+        else{
+                m<-NULL
+                set<-function(y){x<<-ym2
+                                m<<-NULL}
+                get<-function() x
+                solve<-solve(x)
+                setSolve<-function(solve) m<<-solve
+                getSolve<-function() m
+                list(set = set, get = get,setSolve = setSolve,getSolve = getSolve)
+                }
 }
 
 
-## Write a short comment describing this function
+#This function computes the inverse of the special "matrix" returned by makeCacheMatrix above.
+##Or retrieve the inverse if it has been calculated.And print warning massage if the inverse matrix isn't exist.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x,...){
+         if(is.list(x)){
+                m<-x$getSolve()
+                if(!is.null(m)){message("getting cached data")
+                        return(m)}
+                data<-x$get()
+                m<-solve(data,...)
+                x$setSolve(m)
+                m}
+         else if(x==0){message("NOT A Square Matrix,Inverse Matrix isn't exist.")}
+         else if(x==1){message("DET is Zero,Inverse Matrix isn't exist.")}
 }
